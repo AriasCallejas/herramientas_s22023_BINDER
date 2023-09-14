7c48fbd89808fd50dda288578fb32ca3ea1d36e2
AriasCallejas <52322808+AriasCallejas@users.noreply.github.com>
2023-09-14 12:24:36 -0500

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // header file library: permite manejar inputs y outputs
+using namespace std; //permite usar nombres y variables de la libreria estandar
+
+int main()// funcion main, todo lo que esta entre los corchetes {} se ejecuta 
+{
+  cout << "Hello World! \n";
+  cout << "Hello World! "<<endl;
+  return 0;
+} 
+
+
+
+
+
+
+
